e26b8e333143fcb63aaba56f4ae3cc1f6a84d648
Travis1212321 <rekardo647@gmail.com>
2025-07-10 05:05:55 +0300

    Update crypto_utils.py

--- crypto_utils.py
+++ crypto_utils.py
@@ -5,14 +5,19 @@ KEY_FILE = "secret.key"
 
 def generate_key():
     if not os.path.exists(KEY_FILE):
-        with open(KEY_FILE, "wb") as f:
-            f.write(Fernet.generate_key())
+        key = Fernet.generate_key()
+        with open(KEY_FILE, "wb") as key_file:
+            key_file.write(key)
 
 def load_key():
     return open(KEY_FILE, "rb").read()
 
 def encrypt_password(password):
-    return Fernet(load_key()).encrypt(password.encode()).decode()
+    key = load_key()
+    fernet = Fernet(key)
+    return fernet.encrypt(password.encode()).decode()
 
 def decrypt_password(token):
-    return Fernet(load_key()).decrypt(token.encode()).decode()
+    key = load_key()
+    fernet = Fernet(key)
+    return fernet.decrypt(token.encode()).decode()
